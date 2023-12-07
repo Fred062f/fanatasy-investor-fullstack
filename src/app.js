094196ab16eba77ae42fdf213ed9b11259dbd6d3
Frederik <frederik.damsgaard01@gmail.com>
2023-12-07 15:12:21 +0100

    Login, Logout, Register, Stock

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function isAuthenticated(req, res, next) {
 // Set up the Finnhub API key
 // https://finnhub.io/docs/api/quote
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "clfk521r01qovepqafvgclfk521r01qovepqag00"; // API Key is free
+api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
 // Setup EJS
@@ -48,6 +48,7 @@ app.set('views', path.join(__dirname, '../views'));
 // Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -71,6 +72,79 @@ app.get('/stock', (req, res) => {
         // Return JSON response
         res.render('stock', {symbol: symbol, stock: data});
     });
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    // Get body parameters
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Insert user into the database
+    db.query('INSERT INTO users (username, password, balance) VALUES (?, ?, ?)', [username, password, 10000], (err, results) => {
+        if (err) {
+            console.error('Error registering user:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        console.log('User registered successfully')
+        res.render('index')
+    });
+})
+
+app.post('/login', (req, res) => {
+    // Get body parameters
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check user credentials
+    db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+        if (err) {
+            console.error('Error logging in user:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        // If user exists
+        if (results.length > 0) {
+
+            // Regenerate the session to guard against session fixation
+            req.session.regenerate(function (err) {
+                if (err) return next(err);
+
+                // Store user information in the session
+                req.session.user = results[0].username;
+                req.session.userID = results[0].id;
+
+                // Save the session before redirection
+                req.session.save(function (err) {
+                    if (err) return next(err);
+                    console.log('User logged in succesfully')
+                    res.render('stock', {symbol: "", stock: ""})
+                })
+            })
+        }
+    })
+})
+
+app.get('/logout', function (req, res, next) {
+    // logout logic
+
+    // clear the user from the session object and save.
+    // this will ensure that re-using the old session id
+    // does not have a logged in user
+    req.session.user = null
+    req.session.save(function (err) {
+        if (err) next(err)
+
+        // regenerate the session, which is good practice to help
+        // guard against forms of session fixation
+        req.session.regenerate(function (err) {
+            if (err) next(err)
+            res.redirect('/')
+        })
+    })
 })
 
 app.listen(PORT, () => {
